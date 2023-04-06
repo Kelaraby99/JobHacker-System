c8797f26414dd905dcf08e021e8dd1fd3959ba5d
Kelaraby99 <97917618+Kelaraby99@users.noreply.github.com>
2023-04-06 02:28:44 +0200

    Update ArrayFillII.java

--- ArrayFillII.java
+++ ArrayFillII.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
+import java.io.IOException;
 
-public class ArrayFillII{
-    public static void main(String[] args) {
+public class ArraychangeI{
+    public static void main(String[] args) throws IOException{
         Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        int k = 0;
-        while (true) {
-            for (int x = 0; x < T; x++) {
-                System.out.printf("N[%d] = %d\n", k, x);
-                k += 1;
-                if (k == 1000) {
-                    break;
-                }
-            }
-            if (k == 1000) {
-                break;
-            }
+        int[] vetor = new int[20];
+        int i;
+        for (i = 0; i < 20; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+        for (i = 0; i < 20; i++) {
+            System.out.printf("N[%d] = %d\n", i, vetor[19 - i]);
         }
 
     }
