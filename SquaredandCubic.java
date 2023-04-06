4d4bf6595fd16b1231f8b9202396d93b25cb0b3a
Kelaraby99 <97917618+Kelaraby99@users.noreply.github.com>
2023-04-06 02:39:31 +0200

    Update SquaredandCubic.java

--- SquaredandCubic.java
+++ SquaredandCubic.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class SquaredandCubic {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
         for (int i = 1; i <= num; i++){
