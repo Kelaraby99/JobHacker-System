8956886cf169481c0048f0b175a7ac0c8497f206
Kelaraby99 <97917618+Kelaraby99@users.noreply.github.com>
2023-04-06 02:31:28 +0200

    Update Average2.java

--- Average2.java
+++ Average2.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Average2 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
         double a, b, c, med;
 
