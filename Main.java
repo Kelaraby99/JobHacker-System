ea3b92ce96be964cf1d2627a22c0877de74d5837
Kelaraby99 <97917618+Kelaraby99@users.noreply.github.com>
2023-04-06 02:38:33 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         int A, B, X;
         Scanner sc = new Scanner(System.in);
         A = sc.nextInt();
